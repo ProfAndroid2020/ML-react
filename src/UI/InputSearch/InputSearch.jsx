0d699f69b93e75e202b5f3371633855c51280d49
ProfAndroid <sukhanov.licey@gmail.com>
2023-02-16 22:37:15 +0300

    some changes

--- src/UI/InputSearch/InputSearch.jsx
+++ src/UI/InputSearch/InputSearch.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function InputSearch(props) {
+  // some changes
   return (
     <div className="input-field" required>
       <i className="material-icons prefix">search</i>
